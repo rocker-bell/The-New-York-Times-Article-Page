4d9f967579a720abf1bbfe4cab7c561b79b66cd6
rocker-bell <rockbell8@gmail.com>
2025-10-07 14:22:59 +0100

    updates

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import {Routes, Route} from "react-router-dom"
-import './App.css';
+import {Routes, Route} from "react-router-dom";
+
+import Front_page from "./Components/Front-page"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={} />
+      <Route path="/" element={<Front_page/>} />
     </Routes>
   );
 }
